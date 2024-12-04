96e43f84a5b20f260941f9626f6a4e6090732234
Muralitharan <muralitharan.a@spritle.com>
2024-12-04 17:02:58 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
             <Projects />
             <Wrapper>
               <Education />
-              <Contact />
+              {/* <Contact /> */}
             </Wrapper>
             <Footer />
           </div>
