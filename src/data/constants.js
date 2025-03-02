f16e8ac6fa47bc5726ffa7a04be438ba29ffb231
Muralitharan <muralitharan0002@gmail.com>
2025-03-02 16:06:54 +0530

    change: resume url updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -12,7 +12,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/muralitharana",
   resume:
-    "https://drive.google.com/file/d/13D_3QUFSQgOVxS3STKusiPr6z669yyy_/view?usp=drive_link",
+    "https://drive.google.com/file/d/1oyMYAbL5uIjp757uPduYnGRfvfvFynPo/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/muralitharan-a-a49805204",
   mailId:"muralitharan0002@gmail.com",
   mailLink:"https://mail.google.com/mail/?view=cm&fs=1&to=muralitharan0002@gmail.com&su=New opportunity&body=Hi Muralitharan"
